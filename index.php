6806cc73835197c7534d94375d84da794595015b
DenKozlov04 <78017740+DenKozlov04@users.noreply.github.com>
2023-02-10 13:47:27 +0000

    registration form

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+$txt = 'PHP';
+echo "I love $txt!
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Air travel throughout the Baltics</title>
+<meta charset='utf-8' />
+</head>
+<body>
+<h2>Анкета</h2>
+<form action='input.php' method='POST'>
+<p>Введите имя:<br> 
+<input type='text' name='firstname' /></p>
+<form action1='input.php' method='POST1'>
+<p>Введите фамилию:<br>
+<input type='text' name='lastname' /></p>
+<form action2='input.php' method='POST2'>
+<p>Введите почту:<br>
+<input type='text' name='parol' /></p>
+<form action3='input.php' method='POST3'>
+<p>Введите пароль:<br>
+<input type='text' name='post' /></p>
+<p>Форма обучения: <br> 
+<input type='radio' name='eduform' value='очно' />очно <br>
+<input type='radio' name='eduform' value='заочно' />заочно </p>
+<p>Требуется общежитие:<br>
+<input type='checkbox' name='hostel' />Да</p>
+<input type='checkbox' name='hostel' />net</p>
+<p>Краткий комментарий: <br>
+<textarea name='comment' maxlength='200'></textarea></p>
+<input type='submit' value='Отправить'>
+</form>
+</body>
+</html>";
+
+
+echo
+'
+
+'
+
+?>
