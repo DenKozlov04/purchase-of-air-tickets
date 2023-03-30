09b76ad7ad2449dee62ac442e84592edb73732b1
DenKozlov04 <datlovdenis363@gmail.com>
2023-03-30 18:25:35 +0300

    registration

--- check.php
+++ check.php
@@ -1,34 +1,45 @@
 <?php
-   $login = filter_var(trim($_POST['username']),FILTER_SANITIZE_STRING);
+$login = filter_var(trim($_POST['username']),FILTER_SANITIZE_STRING);
 // Удаляем пробелы и HTML символы  
 $email = filter_var(trim($_POST['email']),FILTER_SANITIZE_STRING);
 $phone = filter_var(trim($_POST['phone']),FILTER_SANITIZE_STRING);
 $con_password = filter_var(trim($_POST['con-password']),FILTER_SANITIZE_STRING);
 
 if(mb_strlen($login) <5 || mb_strlen($login) > 90){//если длинна переменной логина меньше чем 5 или длинна логина больше 90 то выводим "ошибку"
-    echo  "Incorect username length";
-    exit();
+    header('Location: registration.html');
+    echo '<script>alert("Incorect username length")</script>';
 }
 
 else if(mb_strlen($email) <2 || mb_strlen($email) > 90){
-    echo  "Incorect email length";
-    exit();
+    header('Location: registration.html');
+    echo '<script>alert("Incorect email length")</script>';
+
 }
 
 else if(mb_strlen($phone) <12 || mb_strlen($phone) > 12){
-    echo  "Incorect phone length";
-    exit();
+    header('Location: registration.html');
+    echo '<script>alert("Incorect phone length")</script>';
+
 }
 
 else if(mb_strlen($con_password) <8 || mb_strlen($con_password) > 32){
-    echo  "Incorect password length (from 8 to 32 symbols)";
-    exit();
+    header('Location: registration.html');
+    echo '<script>alert("Incorect password length (from 8 to 32 symbols)")</script>';
+
+}else {
+    $con_password = md5($con_password."356ads34749ad9s");
+    // хеширование пароля и дополнительное усложнение
+
+    $mysql = new mysqli('localhost','root','','airflights-bd');
+
+    $mysql->query("INSERT INTO `users` (`username`, `email`, `phone` ,`password`, `created_at`) 
+    VALUES('$login', '$email', '$phone', '$con_password', now() )");
+
+    $mysql->close();
+
+    header('Location: index.html');
 }
 
-$mysql = new mysqli('localhost','root','','air_flights_database');
 
-$mysql->query("INSERT INTO`users`(`username,email` ,`password` ,`phone` ,`created_at`) 
-VALUES('$login','$email','$phone','$con_password')");
-
-$mysql->close();
 ?>
+<!-- !!!!НУЖНО ОБЕЗОПАСИТЬ!!!! -->
