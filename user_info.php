1cd1a3b02eb395b26958f2bda7338bf7124d9c41
DenKozlov04 <datlovdenis363@gmail.com>
2023-05-02 16:20:48 +0300

    user page info add

--- user_info.php
+++ user_info.php
@@ -3,6 +3,32 @@
     echo "<h1>Welcome, {$_SESSION['username']}</h1>";
     echo "<h1>Your user ID is {$_SESSION['user_id']}</h1>";
     echo "<p>Your email is {$_SESSION['email']}</p>";
+   
+    // Подключаемся к базе данных
+    $mysqli = new mysqli("localhost", "root", "", "airflightsdatabase");
+    
+    if ($mysqli->connect_error) {
+      die("Connection failed: " . $mysqli->connect_error);
+    }
+    
+    // Получаем данные из таблицы booking
+    $sql = "SELECT bookings.booking_id, bookings.user_id, bookings.flight_id, bookings.booking_date, bookings.seat_number 
+        FROM bookings
+        INNER JOIN users ON bookings.user_id = users.user_id
+        WHERE bookings.user_id = {$_SESSION['user_id']}";
+    
+    $result = $mysqli->query($sql);
+    
+    if ($result->num_rows > 0) {
+      // Выводим данные из таблицы
+      while($row = $result->fetch_assoc()) {
+        echo "Booking ID: " . $row["booking_id"]. " - User ID: " . $row["user_id"]. " - Flight ID: " . $row["flight_id"]. " - Booking Date: " . $row["booking_date"]. " - Seat Number: " . $row["seat_number"]. "<br>";
+      }
+    } else {
+        echo "No bookings found, add booking: <li><a href='Html\Buy_Tickets.php'>ADD</a></li>";
 
-
-    ?> 
+    }
+    
+    $mysqli->close();
+    ?>
+    
